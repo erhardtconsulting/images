42e9533206c05a53272d70f36bb4b7c3a581c7cb
Simon Erhardt <hello@rootlogin.ch>
2024-10-06 13:30:43 +0200

    chore: show directory info

--- freshrss/root/docker-run.sh
+++ freshrss/root/docker-run.sh
@@ -3,6 +3,14 @@
 # Check if /tmp is writeable
 if [ ! -w "/tmp" ]; then
   echo "⛔ Directory '/tmp' is not writeable by the user. Aborting!"
+  echo "Directory Info: $(ls -lhd /tmp)"
+  exit 255
+fi
+
+# Check if /opt/freshrss/data is writeable
+if [ ! -w "/opt/freshrss/data" ]; then
+  echo "⛔ Directory '/opt/freshrss/data' is not writeable by the user. Aborting!"
+  echo "Directory Info: $(ls -lhd /opt/freshrss/data)"
   exit 255
 fi
 
